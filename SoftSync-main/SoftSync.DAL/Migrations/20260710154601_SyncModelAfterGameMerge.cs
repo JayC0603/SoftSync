55c85a6beecd94875e96e849fc5f6f2d8b9650b9
JayC0603 <nguyenminhchanh9224@gmail.com>
2026-07-12 14:47:14 +0700

    Add migration for merged database model

---
+++ SoftSync-main/SoftSync.DAL/Migrations/20260710154601_SyncModelAfterGameMerge.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+#nullable disable
+
+namespace SoftSync.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class SyncModelAfterGameMerge : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            // No-op migration: the DataProtectionKeys table is already created by
+            // 20260710001000_AddDataProtectionKeys. This migration syncs the EF
+            // target model snapshot after the dev merge so startup MigrateAsync
+            // no longer throws PendingModelChangesWarning on Render.
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            // No-op.
+        }
+    }
+}
